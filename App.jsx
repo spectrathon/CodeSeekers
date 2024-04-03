0c194d9defe92fdb7fd1c0fb91f319b539a964e5
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-04-03 13:04:13 +0530

    context created

--- App.jsx
+++ App.jsx
@@ -1,6 +1,8 @@
 import React, { Component, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+
+
 const App = () => {
   return (
     <View style={styles.container}>
@@ -8,14 +10,5 @@ const App = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
-  },
-});
 
 export default App;
